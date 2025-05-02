7277706c9de67554c753ffc51efaa455de13a5be
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-05-02 12:53:19 -0400

    adds publicpath to router

--- src/router/index.js
+++ src/router/index.js
@@ -205,7 +205,7 @@ const dataFetch = async(to, from) => {
 
 const router = createRouter({
   // history: createWebHashHistory(import.meta.env.BASE_URL),
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_PUBLICPATH),
   routes: [
     {
       path: '/:pathMatch(.*)*',
